d88cb3e66ce270c661b0c208187630e0d4ed7a41
Alexander Martin <Alexander@theAlexanderMartin.com>
2020-03-07 14:38:53 -0500

    More map work Added different markers depending on the year and a legend. Also added hover detail windows to each marker, a title to the map, and moved the code for getting ships from the database to wrecks/markers.json Still need to get acutal marker icons instead of these temporary icons from wikipedia, and need to style everything.

--- wrecks/urls.py
+++ wrecks/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='index'),
+    path('wrecks/markers.json', views.markers, name='index'),
 ]
--- wrecks/views.py
+++ wrecks/views.py
@@ -1,11 +1,15 @@
 import os
 import dotenv
+import logging
+
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from .models import Wrecks
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
-from django.shortcuts import render
-
-from .models import Wrecks
+logger = logging.getLogger(__name__)
 
 
 def homepage(request):
@@ -14,3 +18,14 @@ def homepage(request):
         'wrecks': Wrecks.objects.all()
     }
     return render(request, 'wrecks/homepage.html', context)
+
+
+def markers(request):
+    data = []
+    for ship in Wrecks.objects.all():
+        if (ship is not None and ship.latitude is not None and ship.longitude is not None):
+            year = ship.year_sunk if ship.date_sunk is None else ship.date_sunk.year
+            data.append(
+                {"name": ship.ship_name, "num": ship.ship_num, "latitude": float(ship.latitude),
+                 "longitude": float(ship.longitude), "year_sunk": year})
+    return JsonResponse(data, safe=False)
