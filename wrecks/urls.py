ef9426569355aa523995acd96bb3cf91cdf7eed2
basia <blkornoe@mtu.edu>
2020-04-22 13:26:18 -0400

    url update

--- wrecks/urls.py
+++ wrecks/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('auth/logout/', views.logout_view, name='logout'),
     path('favorite', views.changeFavorite),
     path('trivia/', views.trivia, name='index'),
+    path('references/', views.references, name='index'),
 
 ]
